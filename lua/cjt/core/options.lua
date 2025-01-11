e4911fb594fea11f03a53be4dbe27fa825531e75
Carlos J. Torres <carlos.torres@blinkfitness.com>
2025-01-11 11:18:28 -0500

    add tab preferences

--- lua/cjt/core/options.lua
+++ lua/cjt/core/options.lua
@@ -2,6 +2,11 @@ local opt = vim.opt -- for conciseness
 
 opt.autoindent = true
 
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.softtabstop = 2
+
 opt.wrap = false
 
 opt.cursorline = true
